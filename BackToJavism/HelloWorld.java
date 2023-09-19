79cebdd87abbf860da854a099b8fb54ac6b9dfad
adryellsz <adryellpaulo@gmail.com>
2023-09-18 23:26:21 -0300

    the basic

--- BackToJavism/HelloWorld.java
+++ BackToJavism/HelloWorld.java
@@ -1,12 +1,24 @@
 package BackToJavism;
 
+/*
+In this file, I study the basic of programming logig, topics like: Scopes, Access, variables, functions, for loops and
+decision structures
+*/
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("The famous hello world.");
         System.out.println("Prints public age, private age and not famous print below.");
         not_main();
-        System.out.println("Prints ");
+
+        System.out.println("\nThis or that ");
+        this_or_that(12, 13);
+        this_or_that(14, 13);
+        this_or_that(13, 13);
+
+        System.out.println("\n Loops :D");
+        loop();
     }
     public static int public_age = 19;
 
@@ -17,15 +29,26 @@ public class HelloWorld {
         System.out.println("Not famous print output.");
     }
 
-    public static void this_or_that(){
-        int private_age = 21;
-
-        if (private_age > public_age){
-            System.out.println(private_age);
-        } else if (private_age < public_age) {
-            System.out.println(public_age);
+    public static void this_or_that(int age_one, int age_two){
+        if (age_one > age_two){
+            System.out.println(age_one);
+        } else if (age_one < age_two) {
+            System.out.println(age_two);
         } else {
             System.out.println("Private and public age are the same.");
+        }
+    }
+
+    public static void loop(){
+        for (int i = 1; i < 5; i++){
+            System.out.println("Basic for loop.");
+            System.out.println(i);
+
+            boolean must_loop = true;
+            while (must_loop){
+                System.out.println(must_loop);
+                must_loop = false;
+            }
         }
     }
 }
