fce570d0dfb814e00809e0d052c555b8ea47940b
adryellsz <adryellpaulo@gmail.com>
2023-10-22 19:07:50 -0300

    First interaction with spring boot Base strucutre + post route

---
+++ springboot/src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,47 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "PRODUCTS")
+public class ProductModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID productId;
+    private String name;
+    private BigDecimal value;
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
