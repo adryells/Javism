e4d92ac3b52c936ec28dcaf30009f74dc95c8846
adryell <adryellpaulo@gmail.com>
2025-03-03 11:05:34 -0300

    api

---
+++ APIUsersCrud/src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,44 @@
+package com.example.demo.services;
+
+
+import com.example.demo.models.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private com.example.demo.repositories.UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> updateUser(Long id, User userDetails) {
+        return userRepository.findById(id).map(user -> {
+            user.setName(userDetails.getName());
+            user.setEmail(userDetails.getEmail());
+            return userRepository.save(user);
+        });
+    }
+
+    public boolean deleteUser(Long id) {
+        return userRepository.findById(id).map(user -> {
+            userRepository.delete(user);
+            return true;
+        }).orElse(false);
+    }
+}
